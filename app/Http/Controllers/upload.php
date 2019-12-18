1b3a586940d5d89304affea4d48300dcc9224d7b
Subhra Sekhar Mukherjee <subhra.php@gmail.com>
2019-12-18 17:16:53 +0530

    working

--- app/Http/Controllers/upload.php
+++ app/Http/Controllers/upload.php
@@ -86,6 +86,8 @@ class upload extends Controller
      */
     protected function saveFile(UploadedFile $file)
     {
+        echo ini_get('memory_limit').'-----------';
+
         $size = $file->getSize();
         $fileName = $this->createFilename($file);
         // Group files by mime type
@@ -95,10 +97,21 @@ class upload extends Controller
         $filePath = "images/{$mime}/tmp/{$fileName}";
         $realPrivatePath = $file->getRealPath();
         $img = Image::make($realPrivatePath);
+
+        
+        $img->resize(1000, null, function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
+        
+
+        echo $this->convert(memory_get_usage(true)) . "\n";
+
         $img->stream('jpg', 60);
 
+
         if(Storage::disk('public')->put($filePath, $img)){
-            unset($img);
+            $img->destroy();
             if( unlink($realPrivatePath) ){
                 die;
                 return response()->json([
@@ -133,4 +146,116 @@ class upload extends Controller
         $unit=array('b','kb','mb','gb','tb','pb');
         return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
     }
+
+    /**
+     * 
+     * 
+     * $memUsage = $this->getServerMemoryUsage(false);
+        echo sprintf("Memory usage: %s / %s (%s%%)",
+            $this->getNiceFileSize($memUsage["total"] - $memUsage["free"]),
+            $this->getNiceFileSize($memUsage["total"]),
+            $this->getServerMemoryUsage(true)
+        );
+     */
+
+
+    // Returns used memory (either in percent (without percent sign) or free and overall in bytes)
+    protected function getServerMemoryUsage($getPercentage=true)
+    {
+        $memoryTotal = null;
+        $memoryFree = null;
+
+        if (stristr(PHP_OS, "win")) {
+            // Get total physical memory (this is in bytes)
+            $cmd = "wmic ComputerSystem get TotalPhysicalMemory";
+            @exec($cmd, $outputTotalPhysicalMemory);
+
+            // Get free physical memory (this is in kibibytes!)
+            $cmd = "wmic OS get FreePhysicalMemory";
+            @exec($cmd, $outputFreePhysicalMemory);
+
+            if ($outputTotalPhysicalMemory && $outputFreePhysicalMemory) {
+                // Find total value
+                foreach ($outputTotalPhysicalMemory as $line) {
+                    if ($line && preg_match("/^[0-9]+\$/", $line)) {
+                        $memoryTotal = $line;
+                        break;
+                    }
+                }
+
+                // Find free value
+                foreach ($outputFreePhysicalMemory as $line) {
+                    if ($line && preg_match("/^[0-9]+\$/", $line)) {
+                        $memoryFree = $line;
+                        $memoryFree *= 1024;  // convert from kibibytes to bytes
+                        break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (is_readable("/proc/meminfo"))
+            {
+                $stats = @file_get_contents("/proc/meminfo");
+
+                if ($stats !== false) {
+                    // Separate lines
+                    $stats = str_replace(array("\r\n", "\n\r", "\r"), "\n", $stats);
+                    $stats = explode("\n", $stats);
+
+                    // Separate values and find correct lines for total and free mem
+                    foreach ($stats as $statLine) {
+                        $statLineData = explode(":", trim($statLine));
+
+                        //
+                        // Extract size (TODO: It seems that (at least) the two values for total and free memory have the unit "kB" always. Is this correct?
+                        //
+
+                        // Total memory
+                        if (count($statLineData) == 2 && trim($statLineData[0]) == "MemTotal") {
+                            $memoryTotal = trim($statLineData[1]);
+                            $memoryTotal = explode(" ", $memoryTotal);
+                            $memoryTotal = $memoryTotal[0];
+                            $memoryTotal *= 1024;  // convert from kibibytes to bytes
+                        }
+
+                        // Free memory
+                        if (count($statLineData) == 2 && trim($statLineData[0]) == "MemFree") {
+                            $memoryFree = trim($statLineData[1]);
+                            $memoryFree = explode(" ", $memoryFree);
+                            $memoryFree = $memoryFree[0];
+                            $memoryFree *= 1024;  // convert from kibibytes to bytes
+                        }
+                    }
+                }
+            }
+        }
+
+        if (is_null($memoryTotal) || is_null($memoryFree)) {
+            return null;
+        } else {
+            if ($getPercentage) {
+                return (100 - ($memoryFree * 100 / $memoryTotal));
+            } else {
+                return array(
+                    "total" => $memoryTotal,
+                    "free" => $memoryFree,
+                );
+            }
+        }
+    }
+
+    protected function getNiceFileSize($bytes, $binaryPrefix=true) {
+        if ($binaryPrefix) {
+            $unit=array('B','KiB','MiB','GiB','TiB','PiB');
+            if ($bytes==0) return '0 ' . $unit[0];
+            return @round($bytes/pow(1024,($i=floor(log($bytes,1024)))),2) .' '. (isset($unit[$i]) ? $unit[$i] : 'B');
+        } else {
+            $unit=array('B','KB','MB','GB','TB','PB');
+            if ($bytes==0) return '0 ' . $unit[0];
+            return @round($bytes/pow(1000,($i=floor(log($bytes,1000)))),2) .' '. (isset($unit[$i]) ? $unit[$i] : 'B');
+        }
+    }
+
 }
